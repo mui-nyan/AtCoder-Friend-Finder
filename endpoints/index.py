8db006f374685eac7d738aa7280deb655fd89680
むいにゃん <hngrmi@gmail.com>
2020-03-11 07:17:47 +0900

    indexページ表示時のマッチング処理を削除

--- endpoints/index.py
+++ endpoints/index.py
@@ -1,15 +1,11 @@
-from flask import request, render_template, session
+from flask import request, render_template, session,redirect
 from services.twitter import get_friend_users, verify, authorized
 from services.matching import matching
 from repositories.atcoder_user import load_atcoder_users
 
 def get():
 
-    users = None
+    if verify():
+        return redirect("/")
 
-    if verify() or authorized():
-        friends = get_friend_users()
-        atcoder_users = list(load_atcoder_users())
-        users = matching(friends, atcoder_users)
-
-    return render_template('index.html', users=users, authorized=authorized())
+    return render_template('index.html', authorized=authorized())
